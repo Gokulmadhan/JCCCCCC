a8b96209ad16d4659d1654ad653e6496f9864907
Dilipkumar750 <dilipk5406@gmail.com>
2025-08-14 15:05:28 +0530

    Refactor code structure for improved readability and maintainability

--- Backend/server.js
+++ Backend/server.js
@@ -36,5 +36,5 @@ app.use("/orders", ordersRoute);
 app.use("/users", UserRoutes);
 
 // ✅ 5. Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3006;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
